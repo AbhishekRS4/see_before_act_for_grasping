ee152146a65e7a79b8f1a4926ea474f41a3aa921
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2021-11-14 14:34:41 +0100

    updated loss, metrics

--- src/loss_metrics.py
+++ src/loss_metrics.py
@@ -4,21 +4,27 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def compute_dice_loss(pred, label):
+# compute dice loss
+def compute_dice_loss(pred, label, smooth=1e-5):
     pred = pred.contiguous()
     label = label.contiguous()
 
-    intersection = (pred * label).sum(dim=2).sum(dim=2)
+    pred_probs = torch.sigmoid(pred)
+    pred_probs = pred_probs.contiguous()
 
-    loss = (1 - ((2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) + label.sum(dim=2).sum(dim=2) + smooth)))
+    intersection = (pred_probs * label).sum(dim=2).sum(dim=2)
+
+    loss = (1 - ((2. * intersection + smooth) / (pred_probs.sum(dim=2).sum(dim=2) + label.sum(dim=2).sum(dim=2) + smooth)))
 
     return loss.mean()
 
-def compute_bce_loss(pred, label):
-    bce_loss = F.binary_cross_entropy_with_logits(pred, label)
+# compute binary cross entropy loss
+def get_bce_loss():
+    bce_loss = torch.nn.BCEWithLogitsLoss()
 
     return bce_loss
 
+# compute mean pixel accuracy
 def compute_mean_pixel_acc(pred, label):
     if label.shape != pred.shape:
         print("label has dimension", label.shape, ", pred values have shape", pred.shape)
@@ -30,26 +36,21 @@ def compute_mean_pixel_acc(pred, label):
 
     acc_sum = 0
     for i in range(label.shape[0]):
-        label_arr = label[i, :, :, :].clone().detach().cpu().numpy()#.argmax(0)
-        pred_arr = pred[i, :, :, :].clone().detach().cpu().numpy()#.argmax(0)
+        label_arr = label[i, :, :, :].clone().detach().cpu().numpy()
+        pred_arr = pred[i, :, :, :].clone().detach().cpu().numpy()
         pred_arr = pred_arr.astype(np.int32)
 
-        #print(label_arr)
-        #print(pred_arr)
-        #print(np.unique(pred_arr), np.sum(pred_arr))
-        #print(np.unique(label_arr), np.sum(label_arr))
-        #print(label_arr.shape, label_arr.dtype)
-        #print(pred_arr.shape, pred_arr.dtype)
         same = (label_arr == pred_arr).sum()
-        #print(same)
+
         _, a, b = label_arr.shape
         total = a*b
-        #print(total)
+
         acc_sum += same / total
 
     mean_pixel_accuracy = acc_sum / label.shape[0]
     return mean_pixel_accuracy
 
+# compute mean iou (intersection over union)
 def compute_mean_iou(pred, label, smooth=1e-5):
     if label.shape != pred.shape:
         print("label has dimension", label.shape, ", pred values have shape", pred.shape)
@@ -61,14 +62,15 @@ def compute_mean_iou(pred, label, smooth=1e-5):
 
     iou_sum = 0
     for i in range(label.shape[0]):
-        label_arr = label[i, :, :, :].clone().detach().cpu().numpy()#.argmax(0)
-        pred_arr = pred[i, :, :, :].clone().detach().cpu().numpy()#.argmax(0)
+        label_arr = label[i, :, :, :].clone().detach().cpu().numpy()
+        pred_arr = pred[i, :, :, :].clone().detach().cpu().numpy()
         pred_arr = pred_arr.astype(np.int32)
 
-        intersection = np.logical_and(label_arr, pred_arr).sum()
-        union = np.logical_or(label_arr, pred_arr).sum()
-        iou_score = (intersection + smooth) / (union + smooth)
+        intersection = np.logical_and(label_arr, pred_arr).sum() + smooth
+        union = np.logical_or(label_arr, pred_arr).sum() + smooth
+        iou_score = intersection / union
         iou_sum += iou_score
+        print(f"{i}/{label.shape[0]}, pred 1 count : {pred_arr.sum()}, label 1 count : {label_arr.sum()}, intersection : {intersection:.3f}, union : {union:.3f}, iou_score : {iou_score:.3f}")
 
     mean_iou = iou_sum / label.shape[0]
     return mean_iou
