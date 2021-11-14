3b6d03240d377a8b4c75a49c29d0a47c0d46d70c
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2021-11-14 14:34:13 +0100

    updated preprocessing for depth data

--- src/dataset.py
+++ src/dataset.py
@@ -10,12 +10,15 @@ class RGBDGraspAffordanceDataset(Dataset):
     def __init__(self, dir_dataset, transform=None):
         self.dir_dataset = dir_dataset
 
-        self.depth_images_list = [f for f in os.listdir(self.dir_dataset) if f.endswith(".png")]
-        self.color_images_list = [f for f in os.listdir(self.dir_dataset) if f.endswith(".jpg")]
+        self.depth_images_list = [f for f in os.listdir(self.dir_dataset) if f.endswith("depth.png")]
+        self.color_images_list = [f for f in os.listdir(self.dir_dataset) if f.endswith("rgb.jpg")]
         self.labels_list = [f for f in os.listdir(self.dir_dataset) if f.endswith("label.mat")]
 
         self.color_mean = np.array([0.485, 0.456, 0.406]).reshape(1, 3)
         self.color_std = np.array([0.229, 0.224, 0.225]).reshape(1, 3)
+
+        #self.depth_mean = np.array([0.522, 0.522, 0.522]).reshape(1, 3)
+        #self.depth_std = np.array([0.272, 0.272, 0.272]).reshape(1, 3)
 
         self.depth_mean = np.array([0.01, 0.01, 0.01]).reshape(1, 3)
         self.depth_std = np.array([0.03, 0.03, 0.03]).reshape(1, 3)
@@ -35,7 +38,9 @@ class RGBDGraspAffordanceDataset(Dataset):
         color_image = (color_image - self.color_mean) / self.color_std
 
         depth_image = imread(os.path.join(self.dir_dataset, self.depth_images_list[idx]))
-        depth_image = np.repeat(np.expand_dims(depth_image, -1), 3, -1).astype(np.float32)
+        max_depth = np.max(depth_image)
+        depth_image = depth_image.astype(np.float32) / max_depth
+        depth_image = np.repeat(np.expand_dims(depth_image, -1), 3, -1)
         depth_image = (depth_image - self.depth_mean) / self.depth_std
 
         if self.transform:
